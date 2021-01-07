cc4ef5289659b53a6d89e717d048ea38f3a9f07f
Kynan Stevenson <kynants@gmail.com>
2021-01-07 01:31:29 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 9</title>
+</head>
+<body>
+    <!-- https://stackoverflow.com/questions/1691646/php-echo-text-color -->
+    <?php
+    echo '<span style="color:magenta;text-align:center;">P</span>HP Tutorial';
+    ?>
+</body>
+</html>
